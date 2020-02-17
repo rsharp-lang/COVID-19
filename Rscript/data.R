71c13df79624cd40df439236cf8933054cad5d38
xieguigang <xie.guigang@gcmodeller.org>
2020-02-17 17:47:06 +0800

    summary of the raw data

--- Rscript/data.R
+++ Rscript/data.R
@@ -1,3 +1,15 @@
 # Data formatter of the test data
 # https://github.com/BlankerL/DXY-COVID-19-Data/blob/master/csv/DXYArea.csv
 
+require(dataframe);
+
+setwd(!script$dir);
+
+let raw = read.csv("../data/DXYArea.csv", encoding = "utf8");
+
+print("Peeks of the raw data table:");
+print(colnames(raw));
+
+let prov = raw :> as.list(byrow = TRUE);
+
+str(prov);
