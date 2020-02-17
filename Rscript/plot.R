55cb9a6a0633821e674bdc5d0e4e93d9978775eb
xieguigang <xie.guigang@gcmodeller.org>
2020-02-17 16:29:08 +0800

    improvements of the plot details

--- Rscript/plot.R
+++ Rscript/plot.R
@@ -6,16 +6,20 @@ let result <- read.csv(file = "./Kinetics_of_SARS-CoV-2_virus_infection_in_human
 let time <- result[, 1] :> as.numeric;
 let kinetics as double; 
 let names <- list(
-    C = "peoples of cured",
-    T = "peoples of healthy",
-    S = "peoples of suspected infections",
-    I = "peoples of infected",
-    D = "peoples of death"
+    C = ["skyblue", "peoples of cured"],
+    T = ["green",   "peoples of healthy"],
+    S = ["orange",  "peoples of suspected infections"],
+    I = ["red",     "peoples of infected"],
+    D = ["black",   "peoples of death"]
 );
 let plot.kinetics_var as function(var) {
     kinetics <- result[, var] :> as.numeric;
-    serial(time, kinetics, name = names[[var]], color = "red")
-    :> plot
+    serial(time, kinetics, name = (names[[var]])[2], color = (names[[var]])[1])
+    :> plot(
+        padding = [200, 100, 150, 200],
+        x.lab = "time(day)",
+        y.lab = "number of peoples"
+    )
     :> save.graphics(file = `./viz/${var}.png`);
 }
 
