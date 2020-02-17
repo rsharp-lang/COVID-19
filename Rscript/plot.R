bd89f4955249ed6c16d1a38160480a613178a111
xieguigang <xie.guigang@gcmodeller.org>
2020-02-17 16:10:16 +0800

    add serial names data

--- Rscript/plot.R
+++ Rscript/plot.R
@@ -5,9 +5,16 @@ setwd(!script$dir);
 let result <- read.csv(file = "./Kinetics_of_SARS-CoV-2_virus_infection_in_humans.csv");
 let time <- result[, 1] :> as.numeric;
 let kinetics as double; 
+let names <- list(
+    C = "peoples of cured",
+    T = "peoples of healthy",
+    S = "peoples of suspected infections",
+    I = "peoples of infected",
+    D = "peoples of death"
+);
 let plot.kinetics_var as function(var) {
     kinetics <- result[, var] :> as.numeric;
-    serial(time, kinetics, name = var, color = "red")
+    serial(time, kinetics, name = names[[var]], color = "red")
     :> plot
     :> save.graphics(file = `./viz/${var}.png`);
 }
