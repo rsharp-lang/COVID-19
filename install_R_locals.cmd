c67ff9e5c9a17c9e77b2fafd931108741c1e6bbc
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 14:54:53 +0800

    Create install_R_locals.cmd

---
+++ install_R_locals.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+CD App
+
+R# install.packages('COVID-19.dll');
+R# install.packages('R.base.dll');
+R# install.packages('R.graphics.dll');
+R# install.packages('R.math.dll');
+R# install.packages('R.plot.dll');
+
+CD ..
+
+pause
