2ff127aa7d391e0d44d36922801abadbb2ed1053
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 12:48:05 +0800

    Update render_map.R
    test of the css styling

--- Rscript/render_map.R
+++ Rscript/render_map.R
@@ -1,7 +1,11 @@
 imports "COVID.19.map" from "COVID-19.dll";
+imports "grDevices.SVG" from "R.graphics.dll";
 
 # create a blank svg map of china
 let svg = map.china();
+
+# test css styling
+svg :> styles(".Guangxi", list("background-color" = "red"));
 
 svg :> save.graphics(file = "./china.svg");
 
