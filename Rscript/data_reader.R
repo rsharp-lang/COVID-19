55e5b598de83607587aab8676523f3223e71131f
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 13:19:47 +0800

    load raw data table as list

--- Rscript/data_reader.R
+++ Rscript/data_reader.R
@@ -2,17 +2,34 @@ require(dataframe);
 
 let load_raw as function(file.csv) {
     let raw = read.csv.raw(file.csv, encoding = "utf8") :> rows;
-    let names = raw[1] :> which(cell -> cell != "");
-    let types = raw[2];
+    let names = raw[1] :> cells :> which(cell -> cell != "");
+    let types = raw[2] :> cells;
     let data = list();
 
     print(names);
+    print(unique(types));
 
     for(day in raw :> skip(2) :> projectAs(cells)) {
         let date = day[1];
+        let regions = list();
+        let dist;
+        let i = 2; # [date], ....data
+        
+        for(name in names) {
+            dist = list(name = name);
 
+            for(n in 1:3) {
+                dist[[types[i]]] <- day[i];
+                i <- i + 1;
+            }
 
+            regions[[name]] <- dist;
+        }
+
+        data[[date]] <- regions;
     }
+
+    # str(data);
 
     data;
 } 
