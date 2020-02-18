301dd9d3145bdbeb12a89894b03f3d88981f5949
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 11:48:02 +0800

    save blank svg map

--- COVID-19/ChinaMap.vb
+++ COVID-19/ChinaMap.vb
@@ -2,14 +2,14 @@
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.GIS
 Imports Microsoft.VisualBasic.Data.GIS.GeoMap
-Imports Microsoft.VisualBasic.Imaging.SVG
+Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
 <Package("COVID.19.map")>
 Module ChinaMap
 
     <ExportAPI("map.china")>
-    Public Function CreateMap() As SVGDataLayers
-        Return GeoMapRender.Render(CN.GeoChina()).SVG
+    Public Function CreateMap() As SVGData
+        Return GeoMapRender.Render(CN.GeoChina())
     End Function
 End Module
--- Rscript/render_map.R
+++ Rscript/render_map.R
@@ -3,4 +3,5 @@ imports "COVID.19.map" from "COVID-19.dll";
 # create a blank svg map of china
 let svg = map.china();
 
-str(svg :> as.object);
+svg :> save.graphics(file = "./china.svg");
+
