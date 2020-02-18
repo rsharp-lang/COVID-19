1a53a6d9793712079554951c4865bce06ea85fe6
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 14:17:41 +0800

    Update render_map.R

--- Rscript/render_map.R
+++ Rscript/render_map.R
@@ -1,46 +1,55 @@
+# 导入必须的图形库模块
 imports "grDevices.SVG" from "R.graphics.dll";
 imports "COVID.19.map" from "COVID-19.dll";
 
+# 导入外部脚本文件用来读取特定格式的csv文件
 imports "data_reader.R";
 
 setwd(!script$dir);
 
-# create a blank svg map of china
-let svg = map.china();
+# load_raw函数来自于所导入的data_reader.R脚本文件
 let raw = load_raw(file.csv = "../data/DXYArea_simple.csv");
 
-let data.test = raw[["2020-2-4"]];
-let levels = 30;
+let COVID_19.map_render.china as function(day, levels = 30, type = "confirmed") {
+    let data.test <- lapply(raw[[day]], region -> region[[type]]);
+    # create a blank svg map of china
+    let svg = map.china();
 
-# str(data.test);
+    # view data summary
+    str(data.test);
 
-data.test <- lapply(data.test, region -> region$confirmed);
+    let colors = colors("Reds:c6", levels, character = TRUE);
+    # 因为不同省份的数据差异较大
+    # 所以在这里做log转换
+    let values = log( sapply(names(data.test), name -> data.test[[name]])+1);
+    let range = [min(values), max(values)];
 
-# view data summary
-str(data.test);
+    print("Value range in target day:");
+    print(range);
 
-let colors = colors("Reds:c6", levels, character = TRUE);
-let values = log( sapply(names(data.test), name -> data.test[[name]])+1);
-let range = [min(values), max(values)];
+    # do scale mapping to get color level index
+    # percent = (x - min1) / (max1 - min1)
+    # value = percent * (max2 - min2) + min2
 
-print("Value range in target day:");
-print(range);
+    let color.index = round( (levels -1) * ((values - range[1]) / (range[2] - range[1]))) :> as.integer + 1;
+    let i = 1;
 
-# do scale mapping to get color level index
-# percent = (x - min1) / (max1 - min1)
-# value = percent * (max2 - min2) + min2
+    print("Now we translate the stat values to color index");
+    print(color.index);
 
-let color.index = round( (levels -1) * ((values - range[1]) / (range[2] - range[1]))) :> as.integer + 1;
-let i = 1;
+    # do css styling
+    # 为每一个省市通过css添加颜色渲染
+    for(name in names(data.test)) {
+        svg :> styles(`.${name}`, list("fill" = colors[color.index[i]]));
+        i <- i + 1;
+    }
 
-print("Now we translate the stat values to color index");
-print(color.index);
-
-# do css styling
-for(name in names(data.test)) {
-    svg :> styles(`.${name}`, list("fill" = colors[color.index[i]]));
-    i <- i + 1;
+    svg;
 }
 
-svg :> save.graphics(file = "./viz/china.svg");
+["2020-2-18"]
+:> COVID_19.map_render.china(type = "confirmed") 
+:> save.graphics(file = "./viz/2020-2-18.confirmed.svg")
+;
+
 
