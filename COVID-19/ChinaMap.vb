fc356a0c5e852955a4bde5ed7337b7b7959367ee
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 12:28:49 +0800

    AddAdministrativeInformation

--- COVID-19/ChinaMap.vb
+++ COVID-19/ChinaMap.vb
@@ -1,6 +1,7 @@
 ﻿
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.GIS
+Imports Microsoft.VisualBasic.Data.GIS.CN
 Imports Microsoft.VisualBasic.Data.GIS.GeoMap
 Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.Scripting.MetaData
@@ -10,6 +11,6 @@ Module ChinaMap
 
     <ExportAPI("map.china")>
     Public Function CreateMap() As SVGData
-        Return GeoMapRender.Render(CN.GeoChina())
+        Return GeoMapRender.Render(CN.GeoChina()).AddAdministrativeInformation
     End Function
 End Module
