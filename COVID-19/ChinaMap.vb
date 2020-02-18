26e78b9cf65bac6852b384594b571c3aa532e059
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 11:20:59 +0800

    create map data

---
+++ COVID-19/ChinaMap.vb
@@ -0,0 +1,15 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.GIS
+Imports Microsoft.VisualBasic.Data.GIS.GeoMap
+Imports Microsoft.VisualBasic.Imaging.SVG
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("COVID.19.map")>
+Module ChinaMap
+
+    <ExportAPI("map.china")>
+    Public Function CreateMap() As SVGDataLayers
+        Return GeoMapRender.Render(CN.GeoChina()).SVG
+    End Function
+End Module
