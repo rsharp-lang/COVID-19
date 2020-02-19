150a4bc88f71e8e585029cc85c77f4bdd386572a
xieguigang <xie.guigang@gcmodeller.org>
2020-02-19 19:10:40 +0800

    create iterator test success

--- COVID-19/Kinetics_of_SARS_CoV_2_virus_infection_in_humans.vb
+++ COVID-19/Kinetics_of_SARS_CoV_2_virus_infection_in_humans.vb
@@ -1,11 +1,17 @@
 ﻿Imports SMRUCC.Rsharp.Interpreter
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.System.Configuration
 
 Public Class Kinetics_of_SARS_CoV_2_virus_infection_in_humans
 
     Dim modelScript$
+    ''' <summary>
+    ''' Config file path of the R# interpreter
+    ''' </summary>
+    Dim config$
 
-    Sub New(modelScript As String)
+    Sub New(modelScript As String, Optional config$ = Nothing)
+        Me.config = config
         Me.modelScript = modelScript
     End Sub
 
@@ -15,7 +21,12 @@ Public Class Kinetics_of_SARS_CoV_2_virus_infection_in_humans
     ''' <param name="y0"></param>
     ''' <returns></returns>
     Public Function CreateInstance(y0 As InitialStatus)
-        Dim engine As New RInterpreter With {.debug = True}
+        Dim config As Options = If(Me.config.FileExists, New Options(Me.config), Nothing)
+        Dim engine As New RInterpreter(config) With {.debug = True}
+        Dim handle As Action(Of Environment) =
+            Sub(env As Environment)
+
+            End Sub
 
         Call engine.Evaluate(modelScript)
         Call engine.Add("y0", engine.Evaluate($"list(
@@ -25,11 +36,15 @@ Public Class Kinetics_of_SARS_CoV_2_virus_infection_in_humans
             I = {y0.I},  # 最初没有患者
             D = {y0.D}
         )"))
-        Call engine.Add("tick", Sub(env As Environment)
+        Call engine.Add("tick", CObj(handle))
 
-                                End Sub)
-
-        Dim iterator = engine.Evaluate("deSolve(y0, a = 0, b = 31, tick = tick);")
+        Dim iterator = engine.Evaluate("deSolve(
+            Kinetics_of_SARS_CoV_2_virus_infection_in_humans, y0, 
+            a = 0, 
+            b = 31, 
+            resolution = 10000,
+            tick = tick
+        );")
 
         Return iterator
     End Function
--- COVID-19/test/Module1.vb
+++ COVID-19/test/Module1.vb
@@ -3,9 +3,10 @@
 Module Module1
 
     Dim modelFile$ = "E:\COVID-19\Rscript\kinetics\Kinetics_of_SARS_CoV_2_virus_infection_in_humans.R"
+    Dim config$ = "C:\Users\Administrator\AppData\Local\GCModeller\R#\R#.configs.xml"
 
     Sub Main()
-        Dim simulator As New Kinetics_of_SARS_CoV_2_virus_infection_in_humans(modelFile.ReadAllText)
+        Dim simulator As New Kinetics_of_SARS_CoV_2_virus_infection_in_humans(modelFile.ReadAllText, config)
         Dim handle = simulator.CreateInstance(New InitialStatus)
 
         Pause()
