104c1d985f49a0a0f0f5df6619a94d1837b68db3
xieguigang <xie.guigang@gcmodeller.org>
2020-02-19 18:59:19 +0800

    config for test

--- COVID-19/test/Module1.vb
+++ COVID-19/test/Module1.vb
@@ -1,7 +1,13 @@
-﻿Module Module1
+﻿Imports COVID_19
+
+Module Module1
+
+    Dim modelFile$ = "E:\COVID-19\Rscript\kinetics\Kinetics_of_SARS_CoV_2_virus_infection_in_humans.R"
 
     Sub Main()
+        Dim simulator As New Kinetics_of_SARS_CoV_2_virus_infection_in_humans(modelFile.ReadAllText)
+        Dim handle = simulator.CreateInstance(New InitialStatus)
 
+        Pause()
     End Sub
-
 End Module
