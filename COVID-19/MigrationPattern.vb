4a82b02a36e422bb73c60fdb9a2bd73ceb69c85b
xieguigang <xie.guigang@gcmodeller.org>
2020-02-23 13:10:44 +0800

    calculate weights

--- COVID-19/MigrationPattern.vb
+++ COVID-19/MigrationPattern.vb
@@ -1,5 +1,9 @@
-﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 
+''' <summary>
+''' The population migration pattern.
+''' </summary>
 Public Class MigrationPattern
 
     Sub New(flowGraph As NetworkGraph)
@@ -10,6 +14,30 @@ Public Class MigrationPattern
                           Function(g)
                               Return g.ToArray
                           End Function)
-
+        Dim sourceGroup As Dictionary(Of String, Dictionary(Of String, Double)) = flowGraph _
+            .graphEdges _
+            .GroupBy(Function(v) v.U.label) _
+            .ToDictionary(Function(u) u.Key,
+                          Function(g)
+                              Return sourceWeight(g)
+                          End Function)
     End Sub
+
+    Private Shared Function sourceWeight(g As IEnumerable(Of Edge)) As Dictionary(Of String, Double)
+        Dim flowSize As NamedValue(Of Double)() = g _
+            .Select(Function(u)
+                        Return New NamedValue(Of Double) With {
+                            .Name = u.V.label,
+                            .Value = u.data.weight
+                        }
+                    End Function) _
+            .ToArray
+        Dim total As Double = Aggregate target In flowSize Into Sum(target.Value)
+        Dim weights As Dictionary(Of String, Double) = flowSize _
+            .ToDictionary(Function(t) t.Name,
+                          Function(t)
+                              Return t.Value / total
+                          End Function)
+        Return weights
+    End Function
 End Class
