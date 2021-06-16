dd0e2b956f09224a01f371a5e613f4a42e938a80
xieguigang <xie.guigang@gcmodeller.org>
2021-06-16 17:48:16 +0800

    Update data.R

--- R/data/data.R
+++ R/data/data.R
@@ -13,6 +13,8 @@ const getDXYAreaRawData as function() {
     const filepath as string = system.file("data/DXYArea.csv", package = "COVID19");
     const raw = read.csv(filepath, encoding = "utf8");
 
+    raw[, "updateTime"] <- as.Date(raw[, "updateTime"]);
+
     print("Peeks of the raw data table:");
     print("All of the data fields:");
     print(colnames(raw));
@@ -23,49 +25,55 @@ const getDXYAreaRawData as function() {
     raw;
 }
 
-let raw = read.csv("../data/DXYArea.csv", encoding = "utf8");
+#' removes data outside China
+#' 
+const getChinaProvince as function(raw) {
+    const province = raw 
+    |> as.list(byrow = TRUE) 
+    # removes data outside China
+    |> which(r -> r$countryEnglishName == "China")
+    |> groupBy(prov -> prov$provinceEnglishName)
+    ;
 
-raw[, "updateTime"] <- as.Date(raw[, "updateTime"]);
+    print("raw data contains of province data:");
+    cat("\n");
+    str(lapply(province, prov -> length(prov$group), names = prov -> prov$key));
 
+    province;
+}
 
+#' get unique dates
+#' 
+const uniqueDates as function(raw) {
+    let updateTime = sapply(raw[, "updateTime"], as.object);
+    let year       = sapply(updateTime, d -> d$Year);
+    let month      = sapply(updateTime, d -> d$Month);
+    let day        = sapply(updateTime, d -> d$Day);
+    let dates = sprintf("%s-%s-%s", year, month, day) 
+    :> unique 
+    :> as.Date
+    :> orderBy(key -> key)
+    ;
 
-let province = raw 
-:> as.list(byrow = TRUE) 
-# removes data outside China
-:> which(r -> r$countryEnglishName == "China")
-:> groupBy(prov -> prov$provinceEnglishName);
+    print("We have date values:");
+    print(dates);
 
-print("raw data contains of province data:");
-cat("\n");
-str(lapply(province, prov -> length(prov$group), names = prov -> prov$key));
+    dates;
+}
 
-let updateTime = sapply(raw[, "updateTime"], as.object);
-let year       = sapply(updateTime, d -> d$Year);
-let month      = sapply(updateTime, d -> d$Month);
-let day        = sapply(updateTime, d -> d$Day);
-let dates = sprintf("%s-%s-%s", year, month, day) 
-:> unique 
-:> as.Date
-:> orderBy(key -> key);
-
-print("We have date values:");
-print(dates);
-
-cat("\n");
-
-let day.data as function(group) {
+const day.data as function(group) {
     let confirmedCount = sapply(group, x -> x$province_confirmedCount);
-    let curedCount = sapply(group, x -> x$province_curedCount);
-    let deadCount = sapply(group, x -> x$province_deadCount);
+    let curedCount     = sapply(group, x -> x$province_curedCount);
+    let deadCount      = sapply(group, x -> x$province_deadCount);
 
     list(
         province_confirmedCount = max(confirmedCount),
-        province_curedCount = max(curedCount),
-        province_deadCount = max(deadCount)
+        province_curedCount     = max(curedCount),
+        province_deadCount      = max(deadCount)
     );
 }
 
-let province.data as function(prov.group, name) {
+const province.data as function(prov.group, name) {
     # table row subsets
     let updateTime = sapply(prov.group, d -> as.object(d$updateTime));
     let year       = sapply(updateTime, d -> d$Year);
@@ -80,7 +88,8 @@ let province.data as function(prov.group, name) {
     print(`Processing ${length(dates)} dates with ${length(prov.group)} records from '${name}'.`);
 
     let prov  = prov.group :> groupBy(function(d) {
-        i = i + 1; dates[i];
+        i = i + 1; 
+        dates[i];
     });
 
     # print(length(dates));
